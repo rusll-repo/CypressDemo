00beb20b2fb6a43de289bd9c792da21e8647c640
MariaMoroz <82988399+MariaMoroz@users.noreply.github.com>
2022-06-16 21:36:32 -0400

    mm/Add-Remove multiple Elements (#4)
    * Add/Remove multiple Elements

--- cypress/e2e/mm_addElements.cy.js
+++ cypress/e2e/mm_addElements.cy.js
@@ -1,13 +1,34 @@
+const ELEMENTS = 'div #elements';
+const BTN_ADD_ELEMENT = 'button[onclick="addElement()"]';
+
 describe('the-internet.herokuapp.com', () => {
-    it('Add/Remove Elements', () => {
+    
+    it('Add/Remove one Element', () => {
         cy.visit('https://the-internet.herokuapp.com/');
         cy.get('a[href="/add_remove_elements/"]').click();
         cy.get('h3').should('have.text', 'Add/Remove Elements');
 
-        cy.get('button[onclick="addElement()"]').click();
-        let elements = 'div #elements';
-        cy.get(elements).find('button').should('exist');
-        cy.get(elements).find('button').as('Delete').click();
-        cy.get(elements).find('button').should('not.exist');
+        cy.get(BTN_ADD_ELEMENT).click();
+        cy.get(ELEMENTS).find('button').should('exist');
+
+        cy.get(ELEMENTS).find('button').as('Delete').click();
+        cy.get(ELEMENTS).find('button').should('not.exist');
+    })
+
+    it('Add/Remove multiple Elements', () => {
+        let num = 3;
+        for(let i = 0; i < num; i++){
+            cy.get(BTN_ADD_ELEMENT).click();
+        } 
+        cy.get(ELEMENTS).find('button').should('have.length', num);
+
+        cy.get(ELEMENTS).find('button').each(($el) => {
+            if($el.text().includes('Delete')){
+                cy.wrap($el).click();
+                num--;
+                cy.get(ELEMENTS).find('button').should('have.length', num);
+            }
+        })
+        cy.get(ELEMENTS).find('button').should('not.exist');
     })
 })

86ced54c8798e764035b7ff8d9d4b7e514c42214
MariaMoroz <82988399+MariaMoroz@users.noreply.github.com>
2022-06-16 21:43:35 -0400

    refactor assertion (#5)

--- cypress/e2e/mm_firstTest.cy.js
+++ cypress/e2e/mm_firstTest.cy.js
@@ -2,7 +2,7 @@ describe('the-internet.herokuapp.com', () => {
     it('A/B Test Variation 1', () => {
         cy.visit('https://the-internet.herokuapp.com/');
         cy.get('a[href="/abtest"]').click();
-        cy.get('h3').should('have.text', 'A/B Test Variation 1');
+        cy.get('h3').should('contain', 'A/B Test');
         cy.get('#content div p').should('be.visible');
     })
 })

df215d11c101efd60c5256c8df3213e20c960124
Galina R <galina.ruban11@gmail.com>
2022-06-19 19:38:20 -0400

    Dropdown Test Mimic Users Clicks instead of going directly to dropdownpage

--- cypress/e2e/gr_firstTest.cy.js
+++ cypress/e2e/gr_firstTest.cy.js
@@ -1,9 +1,11 @@
 describe('The Internet-Herokuapp tests', () => {
 
   it('Should test dropdown functionality', () => {
-    cy.visit('https://the-internet.herokuapp.com/dropdown');
+    cy.visit('http://the-internet.herokuapp.com/');
+    cy.get('a[href="/dropdown"]').click();
     cy.get('h3').should('have.text', 'Dropdown List');
     cy.get('select').select('Option 2');
     cy.get('select option:selected').should('have.text', 'Option 2')
   })
 })
+

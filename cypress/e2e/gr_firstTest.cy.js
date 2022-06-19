3ca03eaa7b43785272b39c1e6401f23e0e53e99a
Galina R <galina.ruban11@gmail.com>
2022-06-19 19:09:32 -0400

    Dropdown Test One It removed

--- cypress/e2e/gr_firstTest.cy.js
+++ cypress/e2e/gr_firstTest.cy.js
@@ -1,12 +1,8 @@
 describe('The Internet-Herokuapp tests', () => {
 
-  it('Testing dropdown', () => {
-      cy.visit('https://the-internet.herokuapp.com/dropdown');
-      cy.get('h3').should('have.text', 'Dropdown List');
-      cy.get('#dropdown').should('be.visible');
-     })
-
   it('Should test dropdown functionality', () => {
+    cy.visit('https://the-internet.herokuapp.com/dropdown');
+    cy.get('h3').should('have.text', 'Dropdown List');
     cy.get('select').select('Option 2');
     cy.get('select option:selected').should('have.text', 'Option 2')
   })

27974a6c76f4917c80b0a2cb905fec989365a160
Daria <67522929+dashasandler@users.noreply.github.com>
2022-06-23 14:55:50 -0700

    Ds ab test2 (#13)
    * Test for aB testing
    
    * forget to create a branch, cleared master
    
    * Refactoring

--- cypress/e2e/2-advanced-examples/ds_abTest.cy.js
+++ cypress/e2e/2-advanced-examples/ds_abTest.cy.js
@@ -1,18 +1,14 @@
-const ABTESTBTN = 'a[href="/abtest"]';
+const AB_TEST_LINK = 'a[href="/abtest"]';
 
 describe('Herokuapp', () => {
     beforeEach('naigate to AB test', () =>{
       cy.visit('https://the-internet.herokuapp.com/');   
     })
 
-    it('aB test btn exist', () =>{
-      cy.get(ABTESTBTN).should('exist').and('be.visible');  
-    })
-    it('aB test button has a text', () => {
-      cy.get(ABTESTBTN).should('have.text', 'A/B Testing');   
-    })
-    it('AB test text', () =>{
-    cy.get(ABTESTBTN).click();
-    cy.get('h3').should('have.text', 'A/B Test Variation 1');
+    it('aB test link', () =>{
+      cy.get(AB_TEST_LINK).should('exist').and('be.visible');
+      cy.get(AB_TEST_LINK).should('have.text', 'A/B Testing');
+      cy.get(AB_TEST_LINK).click();
+      cy.get('h3').should('contain.text', 'A/B Test');   
     })
 })

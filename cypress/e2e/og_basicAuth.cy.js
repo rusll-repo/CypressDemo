f819e4ceb20ad5b645e04d68c67878f1591f22d6
OlgaG <olyas.goncharenko@gmail.com>
2022-06-30 17:38:05 -0700

    Add test for Basic Auth

---
+++ cypress/e2e/og_basicAuth.cy.js
@@ -0,0 +1,15 @@
+describe('Authorization with proper credentials', () => {
+
+    it('Bypass login', () => {
+        cy.visit('https://the-internet.herokuapp.com/basic_auth', {
+            auth: {
+                username: 'admin',
+                password: 'admin',
+             },
+        })
+        
+        cy.get('#content').should(($div) => {
+            expect($div).to.include.text('Congratulations! You must have the proper credentials.')
+        })
+    })
+})

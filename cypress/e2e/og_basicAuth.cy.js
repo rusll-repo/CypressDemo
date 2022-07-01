ec0290ac23870d7f2ee977935e1bfb390443e420
OlgaG <olyas.goncharenko@gmail.com>
2022-06-30 18:21:22 -0700

    Add new line at the end of the file

--- cypress/e2e/og_basicAuth.cy.js
+++ cypress/e2e/og_basicAuth.cy.js
@@ -7,7 +7,7 @@ describe('Authorization with proper credentials', () => {
                 password: 'admin',
              },
         })
-        
+
         cy.get('#content').should(($div) => {
             expect($div).to.include.text('Congratulations! You must have the proper credentials.')
         })
